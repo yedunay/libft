2cebc529562ab6de789c384f848f8ab3e778a238
Yunus Emre Dunay <ydunay@k2m02s02.42kocaeli.com.tr>
2023-10-15 17:13:33 +0300

    Extras

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ydunay <ydunay@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 09:44:24 by ydunay            #+#    #+#             */
-/*   Updated: 2023/10/15 17:07:21 by ydunay           ###   ########.fr       */
+/*   Updated: 2023/10/15 17:09:59 by ydunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,31 +30,33 @@ static size_t	ft_countword(char const *s, char c)
 	}
 	return (count);
 }
+
 static size_t	ft_word_length(char const *s, char c)
 {
-	size_t len;
-	
+	size_t	len;
+
 	len = 0;
-	while(*s && *s != c)
+	while (*s && *s != c)
 	{
 		len++;
 		s++;
 	}
-	return len;
+	return (len);
 }
 
-static char **ft_free_all(char **result, size_t i)
+static char	**ft_free_all(char **result, size_t i)
 {
-	while(i > 0)
+	while (i > 0)
 		free(result[--i]);
 	free(result);
-	return 0;
+	return (0);
 }
+
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
 	size_t	len;
-	size_t		i;
+	size_t	i;
 
 	result = (char **)ft_calloc((ft_countword(s, c) + 1), sizeof(char *));
 	if (!s || !result)
@@ -64,9 +66,9 @@ char	**ft_split(char const *s, char c)
 	{
 		if (*s != c)
 		{
-			len = ft_word_length(s,c);
+			len = ft_word_length(s, c);
 			result[i] = ft_substr(s, 0, len);
-			if(!result[i])
+			if (!result[i])
 				return (ft_free_all(result, i));
 			i++;
 			s += len;
